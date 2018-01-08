10df1d6ae0c1fa0b557befc65bd9131cd778dc04
Philipp Balzarek <p.balzarek@googlemail.com>
2018-01-08 22:32:50 +0100

    Working root finder, AST using reference counting boxes

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,64 @@
-// // Symbolic functions in 1 variable (R -> R)
-pub enum FN<'a> {
+
+use std::rc::Rc;
+use std::fmt;
+
+// Symbolic functions in 1 variable (R -> R)
+#[derive(Clone, Debug)]
+pub enum FN {
     Var,
     Const(f32),
-    Plus(&'a FN<'a>, &'a FN<'a>),
-    Mult(&'a FN<'a>, &'a FN<'a>)
+    Plus(Rc<FN>, Rc<FN>),
+    Mult(Rc<FN>, Rc<FN>)
 }
 
+impl fmt::Display for FN {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Var => write!(f, "x"),
+            &Const(i) => write!(f, "{}", i),
+            &Plus(ref p, ref q) => write!(f, "({}+{})", p, q),
+            &Mult(ref p, ref q) => write!(f, "{}*{}", p, q)
+        }
+    }
+}
+
+// Helper functions to generate boxed FNs
+fn var() -> Rc<FN> {
+    Rc::new(Var)
+}
+
+fn cnst(i : f32) -> Rc<FN> {
+    Rc::new(Const(i))
+}
+
+fn plus(x : Rc<FN>, y: Rc<FN>) -> Rc<FN> {
+    Rc::new(Plus(x, y))
+}
+
+fn mult(x : Rc<FN>, y: Rc<FN>) -> Rc<FN> {
+    Rc::new(Mult(x, y))
+}
+
+// Generator a polynomial from coefficients (most significant first)
+fn polynomial(v : &Vec<f32>) -> Rc<FN> {
+    if v.len() > 0 {
+        let x0 = v[0];
+        let vs = &v[1..];
+        vs.iter().fold(cnst(x0), |x,y|{plus(mult(x, var()), cnst(*y))} )
+    } else
+    {
+        cnst(0.0)
+    }
+}
 
 use FN::*;
-
-fn sq<'a> (x : &'a FN) -> FN<'a> {
-    Mult(&x,&x)
-}
-
-// fn foldl1<'a, a> (f : &Fn(&a,&a) -> a, v : &Vec<&'a a>) -> &'a a {
-//     let mut acc = v[0];
-//     for w in v[1..].iter() {
-//         acc = &f(&acc, w);
-//     }
-//     acc
-// }
-
-// fn sum<'a>(v: &'a Vec<&'a FN>) -> FN<'a> {
-//     v.iter().fold(Const(0.0), |x : &FN,y : &FN| {Plus(&x,&y)})
-// }
-
-// fn poly<'a>(coeffs : Vec<f32>) -> FN<'a> {
-//     if (coeffs.len() == 0) {
-//         Const(0.0)
-//     }
-//     else {
-//         let mut acc=Const(coeffs[0]);
-//         for c in coeffs[1..].iter() {
-//             let cnst : &'a FN = &Const(*c);
-//             let m = Mult(&acc, &cnst);
-//             acc= m;
-//         }
-//         acc
-//     }
-// }
-
 
 struct AD {
     v : f32, // value
     d : f32 // derivative
 }
 
-// Forward mode derivation
+// Evaluation and forward mode differentiation
 fn ad (f : &FN, x : f32) -> AD {
     match f {
         &Var => AD{ v: x, d: 1.0},
@@ -66,26 +78,27 @@ fn ad (f : &FN, x : f32) -> AD {
 }
 
 
-// Newton-Raphson
+// Newton-Raphson root finding
 fn nr_root(epsilon : f32, x0 : f32, f : &FN) -> f32 {
+    assert!(epsilon > 0.0);
     let mut x = x0;
     let mut c = ad(f, x);
     let mut steps = 0;
-    while (c.v.abs() > epsilon){
+    while c.v.abs() > epsilon {
         c = ad(f, x);
         x -= c.v / c.d;
         steps += 1;
+        // TODO: Conditionally compile or remove
         println!("step {}; x={}; f(x)={}; f'(x)={}  ", steps,  x, c.v, c.d);
+
+        // TODO: Implement error handling
         if steps > 1000 { panic!("Step limit reached")};
     }
     x
 }
 
 fn main() {
-    let v = &Var;
-    let x2 = sq(v);
-    let f = Mult( &x2, &Const(3.0));
-    let x = nr_root(0.0000001, 100.0, &f);
-    println!("root at: {}", x);
-
+    let f = polynomial(&vec![2.0,5.0,3.0,-7.0]);
+    let x = nr_root(1e-6, -100.0, &f);
+    println!("Found root for {} at x={}", f, x);
 }
